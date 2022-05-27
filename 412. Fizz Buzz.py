3f4fb6e6d81f5ba3934ca37fa5c7d4911bfb12a9
dagi_T <dagitensay@gmail.com>
2022-05-27 21:05:04 +0300

    easy

--- 412. Fizz Buzz.py
+++ 412. Fizz Buzz.py
@@ -1,13 +1,14 @@
-n=3
-new=[]
-for i in range(1,n+1):
-    if i % 15==0:
-        new.append('FizzBuzz')
-    elif i%3==0:
-        new.append("Fizz")
-    elif i%5==0:
-        new.append('BUZZ')
-    else:
-        new.append(str(i))
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        new=[]
+        for i in range(1,n+1):
+            if i % 15==0:
+                new.append('FizzBuzz')
+            elif i%3==0:
+                new.append("Fizz")
+            elif i%5==0:
+                new.append('Buzz')
+            else:
+                new.append(str(i))
 
-print(new)
+        return new
